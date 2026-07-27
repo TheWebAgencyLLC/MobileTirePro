e8e31018fe1aebc5f8db2e313260c416a2368e70
Sneh <sneh.mehta23@gmail.com>
2026-07-27 15:25:53 -0500

    feat: adjusted gtm files

--- plugins/vue-gtm.client.js
+++ plugins/vue-gtm.client.js
@@ -1,14 +1,16 @@
 import { createGtm } from "@gtm-support/vue-gtm";
+import { GTM_ID } from "../config/gtm";
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(
     createGtm({
-      id: "GTM-MB6KB9CS",
+      id: GTM_ID,
       defer: false,
       compatibility: false,
       enabled: process.env.NODE_ENV === "production",
       debug: false,
-      loadScript: true,
+      // GTM script is injected in nuxt.config head so Preview mode and Tag Assistant work.
+      loadScript: false,
       vueRouter: useRouter(),
       trackOnNextTick: false,
     })
